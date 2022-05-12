22fa38517e2d73b68086ea255c89d2b09924cf29
BrentSoler <brntsoler@gmail.com>
2022-05-13 02:39:40 +0800

    ui: added contact form /contact-us

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 import TextField from "@mui/material/TextField";
-import { withStyles } from "@mui/material/styles";
-
-const styles = {
-	someTextField: {
-		minHeight: 420,
-	},
-};
+import { Button } from "@mui/material";
 
 const ContactForm = () => {
 	return (
-		<div className="flex flex-col p-5 border-[0.2px] border-gray-500 rounded-md gap-8 w-[90%] md:w-[30rem] md:h-[30rem]">
+		<div className="flex flex-col p-5 border-[0.2px] border-gray-500 rounded-md gap-5 w-[90%] md:w-[30rem] md:h-[30rem] font-pop">
 			<TextField label="Name" sx={{ width: "80%" }} />
 			<TextField label="Contact Number" sx={{ width: "80%" }} />
-			<TextField label="Message" InputProps={{}} />
+			<TextField label="Message" multiline rows={8} />
+			<Button sx={{ width: "10rem", alignSelf: "center" }} variant="contained">
+				Send
+			</Button>
 		</div>
 	);
 };
