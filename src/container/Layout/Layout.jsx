25661cb21c238cb0e67d6c3003eea9ba92b613f2
BrentSoler <brntsoler@gmail.com>
2022-07-22 17:32:28 +0800

    refractor:fixed git conflicts

--- src/container/Layout/Layout.jsx
+++ src/container/Layout/Layout.jsx
@@ -12,7 +12,6 @@ const Layout = ({ children }) => {
 	const [anchor, setAnchor] = useState(null);
 	const location = useLocation();
 	const drawWidth = 220;
-<<<<<<< HEAD
 	const urls =
 		type !== "User"
 			? [
@@ -37,25 +36,6 @@ const Layout = ({ children }) => {
 					},
 					{ name: "Transactions", path: "/transactions" },
 			  ];
-=======
-	const urls = type==="User"?[
-		{
-			name: "Dashboard",
-			path: "/",
-		},
-		{ name: "Transactions", path: "/transactions" },
-	]:[
-		{
-			name: "Dashboard",
-			path: "/",
-		},
-		{
-			name: "Programs",
-			path: "/programs",
-		},
-		{ name: "Transactions", path: "/transactions" },
-	];
->>>>>>> 7c9ee6620348af85741c19241a1e452fec9d707e
 
 	const handleClick = (event) => {
 		setMenuOpen(!menuOpen);
